33766211515d941ac5be04e296b5c8185927c6af
Jenny Cheng <jenny14789632@gmail.com>
2025-07-11 00:22:47 -0700

    Add TimelinePost model and API endpoints for posting and retrieving timeline posts

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 from peewee import *
+from playhouse.shortcuts import model_to_dict 
+import datetime
 
 load_dotenv()
 app = Flask(__name__)
@@ -15,6 +17,16 @@ mydb = MySQLDatabase(
 )
 
 print(mydb)
+
+class TimelinePost (Model) :
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+    class Meta:
+        database = mydb
+mydb. connect ()
+mydb. create_tables([TimelinePost])
 
 hero_info = {
     "name": "Jenny Cheng",
@@ -197,3 +209,22 @@ def hobbies_route():
         hobby_data=hobbies,
         url=os.getenv("URL"),
     )
+
+
+@app. route('/api/timeline_post', methods=['POST' ])
+def post_time_line_post():
+    name = request. form[ 'name']
+    email = request. form['email']
+    content = request.form ['content']
+    timeline_post = TimelinePost. create(name=name, email=email, content=content)
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post() :
+    return {
+        'timeline_posts': [ 
+            model_to_dict(p)
+            for p in
+        TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
