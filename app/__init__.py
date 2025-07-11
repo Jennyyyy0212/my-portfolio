cb5a908c67d08eac5739f70296f73ca1f9c1cc25
Jenny Cheng <jenny14789632@gmail.com>
2025-07-10 23:59:27 -0700

    Fix indentation bug in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,7 @@ from peewee import *
 load_dotenv()
 app = Flask(__name__)
 
-mydb = 
-MySQLDatabase(
+mydb = MySQLDatabase(
     os.getenv("MYSQL_DATABASE"),
     user=os.getenv("MYSQL_USER"),
     password=os.getenv("MYSQL_PASSWORD"),
